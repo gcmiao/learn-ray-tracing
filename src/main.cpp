9d9cffde0852680da71bd466ae367b53e596958c
gcmiao <gc@roastew.com>
2019-10-02 22:18:51 +0200

    Chapter 4. Add sphere

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,23 @@
 using namespace std;
 using namespace glm;
 
+bool hitSphere(const vec3& center, float radius, const Ray& r)
+{
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
 vec3 color(const Ray& r)
 {
-	vec3 unit_direction = normalize(r.direction());
-	float t = 0.5 * (unit_direction.y + 1.0);
+	if (hitSphere(vec3(0, 0, -1), 0.5, r))
+		return vec3(1, 0, 0);
+
+	vec3 unitDirection = normalize(r.direction());
+	float t = 0.5 * (unitDirection.y + 1.0);
 	// blended_value = (1 - t) * start_value + t * end_value
 	return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
@@ -23,7 +36,7 @@ int main()
 	out.open("img.ppm", ios::out | ios::trunc);
 	out << "P3\n" << nx << " " << ny << "\n255\n";
 
-	vec3 lower_left_corner(-2.0, -1.0, - 1.0);
+	vec3 lowerLeftCorner(-2.0, -1.0, - 1.0);
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
@@ -34,7 +47,7 @@ int main()
 		{
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
-			Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
 			vec3 col = color(r);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
